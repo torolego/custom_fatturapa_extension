eb36b6f8b6cc803baf1bdf8e982381dc4e8b3f40
torolego <mattmau@gmail.com>
2025-02-25 09:47:11 +0100

    Update inherited_fatturapa.py
    tested on staging

--- custom_fatturapa_extension/models/inherited_fatturapa.py
+++ custom_fatturapa_extension/models/inherited_fatturapa.py
@@ -1,15 +1,13 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class FatturapaRelatedDocumentTypeInherit(models.Model):
     _inherit = "fatturapa.related_document_type"
 
-    type = fields.Selection(
-        selection_add=[("order", "Determina")],  # Modifica il nome visualizzato
-        ondelete={"order": "cascade"},
-    )
-
-    @api.model
-    def _selection_type(self):
-        """Rimuove la vecchia etichetta e la sostituisce con la nuova"""
-        selection = super()._selection_type()
-        return [(key, "Determina") if key == "order" else (key, value) for key, value in selection]
+  # Sovrascrittura completa del campo type con tutte le opzioni
+    type = fields.Selection([
+        ('order', 'Determina'),  # Modificata solo l'etichetta di 'order'
+        ("contract", "Contract"),
+        ("agreement", "Agreement"),
+        ("reception", "Reception"),
+        ("invoice", "Related Invoice"),
+    ], string='Document Type', required=True)
